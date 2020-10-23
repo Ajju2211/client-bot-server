10e10b9078ff7ec162c32034cd9f1b935873ca45
Ajju2211 <azharuddinm2211@gmail.com>
2020-10-23 11:54:14 +0530

    first

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+// Channel Routes
+// twilio whatsapp channel route
+// const tw_waRouter = require('./routes/tw-waRouter');
+
+// const media_cache = require("./routes/media-cache");
+const app = express();
+
+//Middleware
+//Implement cors
+app.use(cors());
+app.options('*', cors());
+
+//set security HTTPS headers
+// app.use(helmet());
+
+//cookie parser
+app.use(cookieParser());
+//Body parsser, reading  data from body into req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/channels/tw-wa', tw_waRouter);
+
+// app.use('/media/cache', media_cache);
+
+//Handling unexpected routes
+app.all('*', (req, res, next) => {
+	res.status(404).json({
+		status: 'fail',
+		message: `Can't find ${req.originalUrl} on this server`
+	});
+});
+
+module.exports = app;
